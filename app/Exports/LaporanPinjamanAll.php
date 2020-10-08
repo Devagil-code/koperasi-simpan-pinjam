c35bfdbc036d6251735848c3263b5c762bbe2fcf
Asep Saepuloh <warkirasep.puloh@gmail.com>
2020-10-08 11:58:01 +0700

    Add

--- app/Exports/LaporanPinjamanAll.php
+++ app/Exports/LaporanPinjamanAll.php
@@ -42,9 +42,4 @@ class LaporanPinjamanAll implements WithTitle, FromView
             'sum_kredit_pinjaman' => $this->sum_kredit_pinjaman
         ]);
     }
-
-    public function collection()
-    {
-        return $this->transaksi_harian;
-    }
 }
