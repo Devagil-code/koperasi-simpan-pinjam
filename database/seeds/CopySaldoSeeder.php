b9a22f2c8cfa8e2b67cc3b43439f1c553a227345
Rohim <bimzchate@gmail.com>
2020-10-08 15:46:17 +0700

    add seeder

--- database/seeds/CopySaldoSeeder.php
+++ database/seeds/CopySaldoSeeder.php
@@ -14,13 +14,13 @@ class CopySaldoSeeder extends Seeder
     {
         $copy_saldo = new CopySaldo();
         $copy_saldo->from_periode_id = '1';
-        $copy_saldo->to_periode_id = '1';
+        $copy_saldo->to_periode_id = '2';
         $copy_saldo->divisi_id = '1';
         $copy_saldo->status_saldo = '0';
         $copy_saldo->save();
 
         $copy_saldo = new CopySaldo();
-        $copy_saldo->from_periode_id = '2';
+        $copy_saldo->from_periode_id = '1';
         $copy_saldo->to_periode_id = '2';
         $copy_saldo->divisi_id = '2';
         $copy_saldo->status_saldo = '0';
