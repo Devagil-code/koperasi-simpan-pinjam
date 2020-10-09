2a14035ad4a5f70373df1d1dc4f7cade684dd566
Asep Saepuloh <warkirasep.puloh@gmail.com>
2020-10-09 14:06:05 +0700

    Add

--- resources/views/simpanan-debet/index.blade.php
+++ resources/views/simpanan-debet/index.blade.php
@@ -55,7 +55,7 @@
                         <th>Sukarela</th>
                         <th>Keterangan</th>
                         <th>Status Buku</th>
-                        <th>ACTION DEFAULT S</th>
+                        <th>ACTION DEFAULT</th>
                     </tr>
                 </thead>
                 <tbody></tbody>
