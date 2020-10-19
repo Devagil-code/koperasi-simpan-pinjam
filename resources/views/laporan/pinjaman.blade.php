7eb5f300def3333c670081f845aede7645b6452f
Asep Saepuloh <warkirasep.puloh@gmail.com>
2020-10-19 15:19:31 +0700

    Add

--- resources/views/laporan/pinjaman.blade.php
+++ resources/views/laporan/pinjaman.blade.php
@@ -114,63 +114,65 @@
     <div class="col-lg-12">
         <div class="card-box">
             <h4 class="m-t-0 header-title">Laporan Pinjaman</h4>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Tgl</th>
-                        <th>No Anggota</th>
-                        <th>Keterangan</th>
-                        <th>Pinjaman</th>
-                        <th>Cicilan Pinjaman</th>
-                        <th>Akumulasi Bunga</th>
-                        <th>Saldo</th>
-                    </tr>
-                </thead>
-                @if (!empty($transaksi_harian))
-                    <tbody>
-                        @php
-                            $no = 1;
-                            $saldo = 0;
-                            $cicilan = 0;
-                            $sumKredit = 0;
-                            $sumCicilan = 0;
-                            $i = 0;
-                        @endphp
-                        @php
-                            $saldo =  $sum_kredit_pinjaman - ($sum_cicilan + $sum_bunga);
-                        @endphp
+            <div class="table-responsive">
+                <table class="table ">
+                    <thead>
                         <tr>
-                            <th scope="row"></th>
-                            <td></td>
-                            <td>{{ $anggota->nik }}</td>
-                            <td><strong>Saldo Mutasi</strong></td>
-                            <td>{{ Money::stringToRupiah($sum_kredit_pinjaman) }}</td>
-                            <td>{{ Money::stringToRupiah($sum_cicilan) }}</td>
-                            <td>{{ Money::stringToRupiah($sum_bunga) }}</td>
-                            <td>{{ Money::stringToRupiah($saldo) }}</td>
+                            <th>#</th>
+                            <th>Tgl</th>
+                            <th>No Anggota</th>
+                            <th>Keterangan</th>
+                            <th>Pinjaman</th>
+                            <th>Cicilan Pinjaman</th>
+                            <th>Akumulasi Bunga</th>
+                            <th>Saldo</th>
                         </tr>
-                        @foreach ($transaksi_harian as $row)
+                    </thead>
+                    @if (!empty($transaksi_harian))
+                        <tbody>
                             @php
-                                $saldo +=  $row->sumKreditPinjaman->sum('nominal') - ($row->sumCicilan->sum('nominal') + $row->sumBunga->sum('nominal'));
+                                $no = 1;
+                                $saldo = 0;
+                                $cicilan = 0;
+                                $sumKredit = 0;
+                                $sumCicilan = 0;
+                                $i = 0;
+                            @endphp
+                            @php
+                                $saldo =  $sum_kredit_pinjaman - ($sum_cicilan + $sum_bunga);
                             @endphp
                             <tr>
-                                <th scope="row">{{ $no }}</th>
-                                <td>{{ Tanggal::tanggal_id($row->tgl) }}</td>
+                                <th scope="row"></th>
+                                <td></td>
                                 <td>{{ $anggota->nik }}</td>
-                                <td>{{ $row->keterangan }}</td>
-                                <td>{{ Money::stringToRupiah($row->sumKreditPinjaman->sum('nominal')) }}</td>
-                                <td>{{ Money::stringToRupiah($row->sumCicilan->sum('nominal')) }}</td>
-                                <td>{{ Money::stringToRupiah($row->sumBunga->sum('nominal')) }}</td>
+                                <td><strong>Saldo Mutasi</strong></td>
+                                <td>{{ Money::stringToRupiah($sum_kredit_pinjaman) }}</td>
+                                <td>{{ Money::stringToRupiah($sum_cicilan) }}</td>
+                                <td>{{ Money::stringToRupiah($sum_bunga) }}</td>
                                 <td>{{ Money::stringToRupiah($saldo) }}</td>
                             </tr>
-                            @php
-                                $no++;
-                            @endphp
-                        @endforeach
-                    </tbody>
-                @endif
-            </table>
+                            @foreach ($transaksi_harian as $row)
+                                @php
+                                    $saldo +=  $row->sumKreditPinjaman->sum('nominal') - ($row->sumCicilan->sum('nominal') + $row->sumBunga->sum('nominal'));
+                                @endphp
+                                <tr>
+                                    <th scope="row">{{ $no }}</th>
+                                    <td>{{ Tanggal::tanggal_id($row->tgl) }}</td>
+                                    <td>{{ $anggota->nik }}</td>
+                                    <td>{{ $row->keterangan }}</td>
+                                    <td>{{ Money::stringToRupiah($row->sumKreditPinjaman->sum('nominal')) }}</td>
+                                    <td>{{ Money::stringToRupiah($row->sumCicilan->sum('nominal')) }}</td>
+                                    <td>{{ Money::stringToRupiah($row->sumBunga->sum('nominal')) }}</td>
+                                    <td>{{ Money::stringToRupiah($saldo) }}</td>
+                                </tr>
+                                @php
+                                    $no++;
+                                @endphp
+                            @endforeach
+                        </tbody>
+                    @endif
+                </table>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/laporan/simpanan.blade.php
+++ resources/views/laporan/simpanan.blade.php
@@ -112,56 +112,58 @@
     <div class="col-lg-12">
         <div class="card text-center m-b-30">
             <div class="card-body">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Tgl</th>
-                            <th>No Anggota</th>
-                            <th>Keterangan</th>
-                            <th>Pokok</th>
-                            <th>Wajib</th>
-                            <th>Sukarela</th>
-                            <th>Kredit</th>
-                            <th>Saldo</th>
-                        </tr>
-                    </thead>
-                    @if (!empty($transaksi_harian))
-                    <tbody>
-                            @php
-                                $saldo = 0;
-                                $i = 0;
-                            @endphp
-                            @php
-                                $saldo = $sum_pokok + $sum_wajib + $sum_sukarela - $sum_kredit_simpanan;
-                            @endphp
+                <div class="table-responsive">
+                    <table class="table nowrap">
+                        <thead>
                             <tr>
-                                <td></td>
-                                <td>{{ $anggota->nik }}</td>
-                                <td><strong>Saldo Mutasi</strong></td>
-                                <td>{{ Money::stringToRupiah($sum_pokok) }}</td>
-                                <td>{{ Money::stringToRupiah($sum_wajib) }}</td>
-                                <td>{{ Money::stringToRupiah($sum_sukarela) }}</td>
-                                <td>{{ Money::stringToRupiah($sum_kredit_simpanan) }}</td>
-                                <td>{{ Money::stringToRupiah($saldo) }}</td>
+                                <th>Tgl</th>
+                                <th>No Anggota</th>
+                                <th>Keterangan</th>
+                                <th>Pokok</th>
+                                <th>Wajib</th>
+                                <th>Sukarela</th>
+                                <th>Kredit</th>
+                                <th>Saldo</th>
                             </tr>
-                            @foreach ($transaksi_harian as $row)
+                        </thead>
+                        @if (!empty($transaksi_harian))
+                        <tbody>
                                 @php
-                                    $saldo += $row->sumPokok->sum('nominal') + $row->sumWajib->sum('nominal') + $row->sumSukarela->sum('nominal') - $row->sumKredit->sum('nominal');
+                                    $saldo = 0;
+                                    $i = 0;
+                                @endphp
+                                @php
+                                    $saldo = $sum_pokok + $sum_wajib + $sum_sukarela - $sum_kredit_simpanan;
                                 @endphp
                                 <tr>
-                                    <td>{{ Tanggal::tanggal_id($row->tgl) }}</td>
+                                    <td></td>
                                     <td>{{ $anggota->nik }}</td>
-                                    <td>{{ $row->keterangan }}</td>
-                                    <td>{{ Money::stringToRupiah($row->sumPokok->sum('nominal')) }}</td>
-                                    <td>{{ Money::stringToRupiah($row->sumWajib->sum('nominal')) }}</td>
-                                    <td>{{ Money::stringToRupiah($row->sumSukarela->sum('nominal')) }}</td>
-                                    <td>{{ Money::stringToRupiah($row->sumKredit->sum('nominal')) }}</td>
+                                    <td><strong>Saldo Mutasi</strong></td>
+                                    <td>{{ Money::stringToRupiah($sum_pokok) }}</td>
+                                    <td>{{ Money::stringToRupiah($sum_wajib) }}</td>
+                                    <td>{{ Money::stringToRupiah($sum_sukarela) }}</td>
+                                    <td>{{ Money::stringToRupiah($sum_kredit_simpanan) }}</td>
                                     <td>{{ Money::stringToRupiah($saldo) }}</td>
                                 </tr>
-                            @endforeach
-                    </tbody>
-                    @endif
-                </table>
+                                @foreach ($transaksi_harian as $row)
+                                    @php
+                                        $saldo += $row->sumPokok->sum('nominal') + $row->sumWajib->sum('nominal') + $row->sumSukarela->sum('nominal') - $row->sumKredit->sum('nominal');
+                                    @endphp
+                                    <tr>
+                                        <td>{{ Tanggal::tanggal_id($row->tgl) }}</td>
+                                        <td>{{ $anggota->nik }}</td>
+                                        <td>{{ $row->keterangan }}</td>
+                                        <td>{{ Money::stringToRupiah($row->sumPokok->sum('nominal')) }}</td>
+                                        <td>{{ Money::stringToRupiah($row->sumWajib->sum('nominal')) }}</td>
+                                        <td>{{ Money::stringToRupiah($row->sumSukarela->sum('nominal')) }}</td>
+                                        <td>{{ Money::stringToRupiah($row->sumKredit->sum('nominal')) }}</td>
+                                        <td>{{ Money::stringToRupiah($saldo) }}</td>
+                                    </tr>
+                                @endforeach
+                        </tbody>
+                        @endif
+                    </table>
+                </div>
             </div>
         </div>
     </div>
